aac76708b06fc4ae66b11fae97a4f7ac34cb092f
Juan <juaperbar2@alum.us.es>
2019-12-12 18:08:57 +0100

    automatización pruebas

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -72,7 +72,7 @@ MODULES = [
 ]
 
 #BASEURL = 'http://localhost:8000'
-BASEURL = 'http://heroku-meet-travis.herokuapp.com'
+BASEURL = 'http://egc-app3.herokuapp.com'
 
 APIS = {
     'authentication': BASEURL,
